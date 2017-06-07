0e68cb136d51fe729c752b458e7b8f1450203f64
georgezsiga <georgezsiga@gmail.com>
2017-06-07 12:04:30 +0200

    a1

--- TribesOfLagopusAndroid/src/com/greenfox/Main.java
+++ TribesOfLagopusAndroid/src/com/greenfox/Main.java
@@ -2,7 +2,12 @@ package com.greenfox;
 
 public class Main {
 
+    public void greet() {
+        System.out.println("Hi Geoge!");
+    }
+
     public static void main(String[] args) {
-	// write your code here
+	  Main main = new Main();
+	  main.greet();
     }
 }
